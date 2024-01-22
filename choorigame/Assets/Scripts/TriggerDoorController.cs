bab162bb2bb5c6f6a18d90b5d50ac7c8a2830cfd
Jiyeon Na <skwldus@hanyang.ac.kr>
2024-01-22 21:28:38 +0900

    add: 1인칭 카메라

--- choorigame/Assets/Scripts/TriggerDoorController.cs
+++ choorigame/Assets/Scripts/TriggerDoorController.cs
@@ -10,7 +10,7 @@ public class TriggerDoorController : MonoBehaviour
     private bool isOpen1 = false;
     private bool isOpen2 = false;
 
-    public float interactionRange = 1f;
+    public float interactionRange = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,24 +24,32 @@ public class TriggerDoorController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {   
+            // Debug.Log("E pressed");
             float distanceDoor1 = Vector3.Distance(transform.position, doorAnimator1.transform.position);
             float distanceDoor2 = Vector3.Distance(transform.position, doorAnimator2.transform.position);
+
+            Debug.Log("Distance to door1: " + distanceDoor1);
+            Debug.Log("Distance to door2: " + distanceDoor2);
 
             if(distanceDoor1 <= interactionRange){
                 if(!isOpen1){
                     doorAnimator1.SetTrigger("DoorOpen1");
+                    // Debug.Log("DoorOpen1");
                 }
                 else{
                     doorAnimator1.SetTrigger("DoorClose1");
+                    // Debug.Log("DoorClose1");
                 }
                 isOpen1 = !isOpen1;
             }
             if(distanceDoor2 <= interactionRange){
                 if(!isOpen2){
                     doorAnimator2.SetTrigger("DoorOpen2");
+                    // Debug.Log("DoorOpen2");
                 }
                 else{
                     doorAnimator2.SetTrigger("DoorClose2");  
+                    // Debug.Log("DoorClose2");
                 }
                 isOpen2 = !isOpen2;
             }
