d56c48f232ba9e37040cb40963070531d21f1cbe
ParkGangtae <rkdxo0417@gmail.com>
2024-01-24 15:20:01 +0900

    add: Scene Switcher
    scene전환 조명 문제 없음!

--- choorigame/Assets/Scripts/InteractionSystem/dead_body.cs
+++ choorigame/Assets/Scripts/InteractionSystem/dead_body.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class dead_body : MonoBehaviour, IInteractable
-{
+{   
+    public SceneSwitcher sceneSwitcher;
     [SerializeField] private string _prompt;
+    void Start(){
+        if (sceneSwitcher == null){
+            sceneSwitcher = FindObjectOfType<SceneSwitcher>();
+        }
+    }
     public string InteractionPrompt{
         get{
             return _prompt;
@@ -20,6 +27,13 @@ public class dead_body : MonoBehaviour, IInteractable
 
     public bool Interact (InteractionControllers interactor){
         Debug.Log("dead_body");
+        StartCoroutine(WaitAndSwitchScene(2f));
         return true;
     }
+
+    private IEnumerator WaitAndSwitchScene(float waitTime){
+        yield return new WaitForSeconds(waitTime);
+        sceneSwitcher.SwitchToScene2();
+    }
+
 }
---
+++ choorigame/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public int scene1Index;
+    public int scene2Index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SceneManager.LoadScene(scene1Index);
+    }
+
+    // Update is called once per frame
+    public void SwitchToScene2()
+    {
+        SceneManager.LoadScene(scene2Index);
+    }
+}
